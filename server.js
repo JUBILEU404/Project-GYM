fe4ab6625872d84010cadd79230dde60a49fd11b
JUBILEU404 <locaytonfenix@gmail.com>
2023-09-12 09:58:59 -0300

    Adicionado Arquivos de Configuração

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+
+var corsOptions = {
+
+     origin: "http://localhost:8081"
+
+
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+const db = require("./app/models");
+db.Sequelize.sync()
+ .then(() => {
+    console.log("banco conectado!")
+ })
+ .catch((err) =>{
+    console.log("Falha ao acessar banco de dados: "+ err.message)
+ });
+
+ require("./app/routes/item.routes")(app);
+
+app.get("/" , (req,res) => { 
+    res.json({nessage: 'Hello World!!' })
+
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server funcionando na porta ${PORT}.`);
+
+});
